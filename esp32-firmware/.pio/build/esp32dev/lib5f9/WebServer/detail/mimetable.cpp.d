c53db2f52dd514f76dd159004c7b925032f616ad
theanh8211 <trantheanh.12a3@gmail.com>
2026-01-08 12:45:21 +0700

    Clean repo: ignore PlatformIO build files

--- esp32-firmware/.pio/build/esp32dev/lib5f9/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib5f9/WebServer/detail/mimetable.cpp.o: \
- /root/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /root/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /root/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
